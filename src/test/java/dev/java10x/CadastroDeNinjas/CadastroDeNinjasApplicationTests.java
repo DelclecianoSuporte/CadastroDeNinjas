9a81b5dda7b9f11bdfda79fe1afc43db41ca440d
delclecianojunior <delclecianobj@hotmail.com>
2026-06-24 15:07:37 -0300

    Apontando a classe de testes para a DEV

--- src/test/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
+++ src/test/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -2,8 +2,10 @@ package dev.java10x.CadastroDeNinjas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class CadastroDeNinjasApplicationTests {
 
 	@Test
